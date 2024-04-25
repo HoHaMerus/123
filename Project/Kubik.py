c4fe8809db4fb905cb30a7d2bf2fe2e0f91504e5
HoHaMerus <HoHaMe@yandex.ru>
2024-04-25 16:36:00 +0300

    Бросок Кубика

---
+++ Project/Kubik.py
@@ -0,0 +1,49 @@
+#Имитация броска кубика
+
+import random
+import os
+
+#Выбор колличества кубиков
+
+def brosok():
+  while True:
+      try:
+          kol_kubikov = input("Количество кубиков: ")
+          otvet = ["1", "2"]
+          if kol_kubikov not in otvet:
+              raise ValueError("Можно выбрать 1 или 2")
+          else:
+              return kol_kubikov
+      except ValueError as er:
+          print(er)
+
+
+def brosok_kubikov():
+   min = 1
+   max = 6
+   povtor = "y" 
+
+   while povtor.lower() =="y":
+       os.system("cls" if os.name == "nt" else "clear")
+       amount = brosok()
+
+       if amount == "2":
+           print("Выпало: ")
+           kubik1 = random.randint(min, max)
+           kubik2 = random.randint(min, max)
+
+           print("Итог: ")
+           print("Первый кубик: ", kubik1)
+           print("Второй кубик: ", kubik2)
+           print("Результат: ", kubik1 + kubik2)
+
+           povtor = input("Бросаем еще: ")
+       else:
+           kubik1 = random.randint(min, max)
+           print(f"Результат: {kubik1}")
+
+           povtor = input("Бросаем еще: ")
+
+
+if __name__ == '__main__':
+   brosok_kubikov()
