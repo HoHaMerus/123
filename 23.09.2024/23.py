43c8f5ce20c8fed43b668b57eb55637d325e56c5
HoHaMerus <HoHaMe@yandex.ru>
2024-09-24 17:30:27 +0300

    2 задача

---
+++ 23.09.2024/23.py
@@ -0,0 +1,3 @@
+s = "Word to Split"
+wordlist = list(s)              
+wordlist = [ch for ch in s] 
---
+++ 23.09.2024/test.py
@@ -0,0 +1,3 @@
+old_list = ["apple", "banana", "cherry"]
+new_list = str(old_list).replace(",", "")
+print(new_list)
