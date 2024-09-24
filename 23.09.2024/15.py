ef8e8538f7b6bad119201d564c8fa8ea45667ff5
HoHaMerus <HoHaMe@yandex.ru>
2024-09-24 12:56:10 +0300

    Положительное ил отрицательное число

---
+++ 23.09.2024/15.py
@@ -0,0 +1,6 @@
+chislo=int(input("Введите число: "))
+while chislo!=0:
+    if chislo>=0: print("Это положительное число")
+    else: print("Это отрицательное число")
+    chislo = int(input("Введите число: "))
+print(chislo)
