e1b6a0b4d2c2d4bab33c9ee9d5b44fc258b73aef
HoHaMerus <HoHaMe@yandex.ru>
2024-09-25 13:23:54 +0300

    1

--- 23.09.2024/23.py
+++ 23.09.2024/23.py
@@ -1,4 +1,10 @@
-a = []
-a=input("Введите фразу:")
-a=a.split()
-print(a)
+# s1 = list(input('Введите строку: ').lower())
+#
+# s2=[]
+# for i in s1:
+#     if i!=' ':
+#         s2.append(i)
+# print(s2)
+
+s2=list(input().lower())
+print([i for i in s2 if i!=' '])
