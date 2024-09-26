b1f59a94119e601ed362d609f693ef0ae5a4d59c
HoHaMerus <HoHaMe@yandex.ru>
2024-09-26 21:00:28 +0300

    3 задача

--- 23.09.2024/46.py
+++ 23.09.2024/46.py
@@ -1,7 +1,6 @@
-dict1={"id324": 167,"id95": 204,"id326": 45,"id327": 90,"id328": 3,"id329": 43,"id405": 27,"id567": 10}
-dict2=dict1.copy()
-for key, value in dict2.items:
-    if value % 2 == 0:
+dict1 = {'id324': 167, 'id95': 204, 'id326': 45, 'id327': 90, 'id328': 3, 'id329': 43, 'id405': 27, 'id567': 10}
+dict2 = dict1.copy()
+for key in list(dict2.keys()):
+    if dict2[key] % 2 == 0:
         del dict2[key]
-keys=list(dict2.keys)
-print(dict2)
+print(list(dict2.keys()))
